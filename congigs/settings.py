22f29614568fadb87ce4969e328cedfbf4be13cc
Yegane Gholipour <yeganegholiour@gmail.com>
2024-02-10 18:08:48 +0330

    add : .env and configurations to the setting

--- congigs/settings.py
+++ congigs/settings.py
@@ -11,19 +11,26 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+
+# Load environment variables from .env file
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-g+av*r5lu2)(jom7rv1&dautxg!6m4w7c^u)91t=9s&q7-4aek"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -43,6 +50,7 @@ INSTALLED_APPS = [
 
     "user", 
     "board",
+    'goal',
 ]
 
 MIDDLEWARE = [
@@ -129,13 +137,13 @@ USE_TZ = True
 
 
 # Email Backend Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')  
 EMAIL_HOST = "smtp.gmail.com"  
 EMAIL_PORT = 587  
 EMAIL_USE_TLS = True  
-EMAIL_HOST_USER = "BookClubSystemEmail@gmail.com"  
-EMAIL_HOST_PASSWORD = "wxxjzuovtlilaucw "
-DEFAULT_FROM_EMAIL = "BookClubSystemEmail@gmail.com" 
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')    
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')   
 
 
 # Static files (CSS, JavaScript, Images)
